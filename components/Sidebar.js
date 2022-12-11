4129125fc51554e8684ad3cd286c6bb9344047d6
thorcazo <thorcazodesign@gmail.com>
2022-12-11 15:07:06 +0100

    First Commit

---
+++ components/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <Image src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" 
+                width={200} 
+                height={100}
+                alt="Spotify" 
+                
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
